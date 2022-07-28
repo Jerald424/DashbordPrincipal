23cd4d1d8abce0a26c2090c3ff1998acc6a010f2
stephenjerald <stephenjerald@dbcyelagiri.edu.in>
2022-07-28 17:52:19 +0530

    issue fixed

--- src/Pages/student/Student.js
+++ src/Pages/student/Student.js
@@ -1,54 +1,54 @@
 import React from 'react'
-import Main from '../Main'
+// import Main from '../Main'
 
 export default function Student() {
     return (
         <div>
-            <Main>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1> <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1> <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1> <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1> <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1> <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-                <h1>hello student</h1>
-            </Main>
+            {/* <Main> */}
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1> <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1> <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1> <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1> <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1> <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            <h1>hello student</h1>
+            {/* </Main> */}
         </div>
     )
 }
